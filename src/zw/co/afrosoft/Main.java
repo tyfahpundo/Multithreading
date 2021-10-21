0fe40a717907f99dddb04ac2b5b2672b019b147d
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-21 12:06:23 +0200

    Created a new Thread

--- src/zw/co/afrosoft/Main.java
+++ src/zw/co/afrosoft/Main.java
@@ -2,7 +2,26 @@ package zw.co.afrosoft;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+	    Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are now in thread "+Thread.currentThread().getName());
+            }
+        });
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread "+t.getName()+"the error is "+e.getMessage());
+            }
+        });
+
+        thread.setName("New worker thread");
+        System.out.println("We are in thread: "+ Thread.currentThread().getName()+ " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: "+ Thread.currentThread().getName()+ " after starting a new thread");
+
+        Thread.sleep(10000);
     }
 }
